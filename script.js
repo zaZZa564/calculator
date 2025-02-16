aec25fdc40ad07cd0a9cc2e60debe25bb178abce
zaZZa564 <anus_bobra564@mail.ru>
2025-02-16 07:46:23 +0300

    generation buttons in script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// generate buttons
+const numbersContainer = document.querySelector('.numbersContainer');
+for(let i = 0; i <= 9; i++) {
+  const button = document.createElement('button');
+  button.classList = 'numberButton';
+  button.textContent = i;
+  button.addEventListener('click', () => {
+    console.log(`pressed button ${i}`);
+  });
+  numbersContainer.appendChild(button);
+}
+
 let a;
 let b;
 let operation;
