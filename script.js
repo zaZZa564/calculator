df7b54c28c461172641998087b8856d2a0684330
zaZZa564 <anus_bobra564@mail.ru>
2025-02-16 08:31:27 +0300

    displays for numbers and operations

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
+// variables for operations
+let a;
+let b;
+let operation;
+
 // generate buttons
 const numbersContainer = document.querySelector('.numbersContainer');
+const display = document.querySelector('.display');
 for(let i = 0; i <= 9; i++) {
   const button = document.createElement('button');
   button.classList = 'numberButton';
   button.textContent = i;
   button.addEventListener('click', () => {
-    console.log(`pressed button ${i}`);
+    display.textContent = i;
   });
   numbersContainer.appendChild(button);
 }
 
-let a;
-let b;
-let operation;
+const operationsContainer = document.querySelector('.operationsContainer');
+const displayOperation = document.querySelector('.displayOperation');
+for(let i = 1; i <= 4; i++) {
+  const button = document.createElement('button');
+  switch(i) {
+    case 1: button.textContent = '+'; break;
+    case 2: button.textContent = '-'; break;
+    case 3: button.textContent = '*'; break;
+    case 4: button.textContent = '/'; break;
+  }
+  button.addEventListener('click', () => {
+    displayOperation.textContent = button.textContent;
+    operation = button.textContent;
+  });
+  operationsContainer.appendChild(button);
+}
 
+// functions
 function operate(a, b, operation) {
   switch(operation) {
     case '+': return a + b;
